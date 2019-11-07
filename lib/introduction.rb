dea82f04bcf5ca260004e95e53044b2d6244515d
serena Lee <dearserenalee@gmail.com>
2019-11-07 03:50:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++
@@ -1,8 +0,0 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
-end
-introduction("Josh")
-
-def introduction(name, language)
-  puts "Hi my name is #{name} and I am learning to program #{language}."
-end
