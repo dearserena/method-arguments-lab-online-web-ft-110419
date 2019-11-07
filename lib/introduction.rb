5a90b2f28e5d4e301413162c4469c47d563bd035
serena Lee <dearserenalee@gmail.com>
2019-11-07 03:47:01 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,7 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+introduction()
 
 def introduction(name, language)
   puts "Hi my name is #{name} and I am learning to program #{language}."
